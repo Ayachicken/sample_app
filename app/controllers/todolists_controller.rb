72d6a38597270e8f94aea63cdf334a3a5dafc9d5
Ayachicken <digimonmon.a@gmail.com>
2020-04-12 01:50:36 +0900

    [finish]1-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -2,4 +2,23 @@ class TodolistsController < ApplicationController
   def new
   	@list = List.new
   end
+
+  def creat
+
+  	list = List.new(list_params)
+
+  	list.save
+
+  	redirect_to '/top'
+
+  end
+
+  private
+
+  def list
+
+  	params.require(:list).parmit(:title,:body)
+  	
+  end
+
 end
